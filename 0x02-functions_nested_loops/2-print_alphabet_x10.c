99814f632b09adf79178c4dd30df33fa36fb9d9d
Achref-haddaji <haddaji.achref19@gmail.com>
2022-02-08 12:48:52 +0100

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet functions 10 times.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+char az = 'a';
+	int count = 1;
+
+	while (count < 11)
+	{
+		while (az <= 'z')
+		{
+			_putchar(az);
+			az++;
+		}
+		_putchar('\n');
+		az = 'a';
+		count++;
+	}
+}
