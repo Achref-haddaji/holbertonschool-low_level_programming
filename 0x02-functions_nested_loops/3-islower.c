5edbffdbd913c1db5aff5ecb10b7e50ddc566328
Achref-haddaji <haddaji.achref19@gmail.com>
2022-02-08 14:33:20 +0100

    task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - function that checks for lowercase character.
+ * Return : always 0
+ */
+int _islower(int c) 
+{
+if ( c >= 'a' && c <= 'z')
+{
+ return (0);
+	/* code */
+}
+else
+{
+	return (0);
+}
+
+}
