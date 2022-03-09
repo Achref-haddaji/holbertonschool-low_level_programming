81a6bb51ade16d8595246f636f128cc19cc40162
Achref-haddaji <haddaji.achref19@gmail.com>
2022-03-09 10:42:29 +0100

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints the name given.
+ * @name: input name.
+ * @f: function that print input;
+ * Return: no return.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	void (*pfn)(char *);
+
+	pfn = f;
+	pfn(name);
+}
