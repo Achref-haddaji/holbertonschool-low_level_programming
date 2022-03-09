902b0eb892314dac1ea3837eed80dff2098d3c5a
Achref-haddaji <haddaji.achref19@gmail.com>
2022-03-09 12:15:55 +0100

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,13 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
- * print_name - prints a name
- * @name: pointer to name
- * @f: pointer to function
- * Return: void
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: function
+ * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f)
+		f(name);
 }
