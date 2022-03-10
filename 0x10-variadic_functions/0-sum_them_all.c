4c2b1a3002a3816a9740466b6caa5fdb9cde61c2
Achref-haddaji <haddaji.achref19@gmail.com>
2022-03-10 12:33:28 +0100

    task0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,26 +1,24 @@
 #include "variadic_functions.h"
 /**
- * sum_them_all - sums all parameters
- * @n: integer
- * @...: functions
- * Return: int
+ * sum_them_all - calculate the sum of a list of numbers
+ * @n: the number of element to add
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-
+	int k = 0;
 	unsigned int i;
-	int sum = 0;
-
-	if (n == 0)
-		return (0);
 
 	va_start(args, n);
-
+	if (n == 0)
+		return (0);
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(args, unsigned int);
+		k += va_arg(args, int);
 	}
 	va_end(args);
-	return (sum);
+return (k);
 }
