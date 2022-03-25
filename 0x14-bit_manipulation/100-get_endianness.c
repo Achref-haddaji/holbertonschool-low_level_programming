72e18f5b6fd7f7edf8b5fc42ef38cc0f66cc16a2
Achref-haddaji <haddaji.achref19@gmail.com>
2022-03-25 10:12:13 +0100

    task6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* get_endianness - the function
+*
+*Return: ...
+*
+*/
+
+int get_endianness(void);
+{
+unsigned int x = 1;
+char *c;
+
+c = (char *)&x;
+if (*c)
+return (1);
+else
+return (0);
+}
