a10debf9a1336fba458ece0304300dffe7c02db3
Achref-haddaji <haddaji.achref19@gmail.com>
2022-05-05 12:28:36 +0100

    task0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - print dlistint_t list
+ *@h: pointer to the start of the linked list
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t index;
+
+	for (index = 0; h != NULL; index++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (index);
+}
